cd8316462354f1399f780c879f5cbc797649970b
Muhammad Sagheer <125592412+Sagheer2003@users.noreply.github.com>
2023-11-05 17:42:38 +0500

    Add files via upload
    Update contant

---
+++ Assingment NO 2/app.js
@@ -0,0 +1,74 @@
+//Question # 01
+
+var number1 = 29;
+var number2 = 19;
+// alert(number1 + number2);
+
+var title1 = "Question # 01"
+
+var sum1 = number1 + number2;
+var contant = "Sum of 29 and 19 is ";
+
+alert(title1 + "\n" + contant + sum1);
+
+//Question # 02
+
+var numsub1 = 29;
+var numbsub2 = 19;
+var title2 = "Queston # 02"
+
+var subsum = numsub1 - numbsub2;
+var sum2 = "Result is Subtracting 29 to 19 is "
+
+
+
+alert(title2 + "\n"  +sum2 + subsum);
+
+
+
+
+
+//Question # 2a
+
+var nummul1 = 5;
+var numbmul2 = 6;
+
+var title3 = "Question # 2(a)"
+
+var digitSum = nummul1 * numbmul2;
+
+var mulSum2 = "Result of  Multipleying 5 to 6 is "
+
+
+
+alert( title3 + "\n" + mulSum2 + digitSum );
+
+//Question # 2 (b)
+
+var numDiv1 = 5;
+var numDiv2 = 3;
+
+var title4 = "Question # 2(b)"
+
+var divdSum = numDiv1 / numDiv2;
+
+var divSum = "Result of  dividing 5 to 3 is "
+
+
+
+alert( title4  + "\n" + divSum  +  numDiv1 + numDiv2 + divdSum );
+
+//Question # 2 (c)
+
+var divide1 = 6;
+var divide2 = 2;
+
+var title5 = "Question # 2(c)"
+
+var divdSum =divide1  / divide2 ;
+
+var divideSum = "Remainder of dividing 6 to 2 is "
+
+
+
+alert( title5  + "\n" + divideSum  + +divdSum );
